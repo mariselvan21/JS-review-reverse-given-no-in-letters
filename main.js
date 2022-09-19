2d6001ae8d1069defe04b306be59f0d41f38f5a6
c.mariselvan <mari20041999@gmail.com>
2022-09-19 14:26:38 +0530

    add files

---
+++ main.js
@@ -0,0 +1,60 @@
+var number = parseInt(prompt('Enter any number'));
+console.log("Given number is " + number);
+var temp = number;
+while (temp) {
+    lastDigit = temp % 10;
+    switch (lastDigit) {
+        case 0:
+            {
+                console.log('zero');
+                break;
+            }
+        case 1:
+            {
+                console.log('one');
+                break;
+            }
+        case 2:
+            {
+                console.log('two');
+                break;
+            }
+        case 3:
+            {
+                console.log('three');
+                break;
+            }
+        case 4:
+            {
+                console.log('four');
+                break;
+            }
+        case 5:
+            {
+                console.log('five');
+                break;
+            }
+        case 6:
+            {
+                console.log('six');
+                break;
+            }
+        case 7:
+            {
+                console.log('seven');
+                break;
+            }
+        case 8:
+            {
+                console.log('eight');
+                break;
+            }
+        case 9:
+            {
+                console.log('nine');
+                break;
+            }
+
+    }
+    temp=(temp-lastDigit)/10;
+}
